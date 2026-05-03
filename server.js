dbf0771b188b9817eadac1f63f4c1ead9364dfb9
Naoman Saeed <naoman.saeed@gmail.com>
2026-05-03 08:19:54 +0500

    feat: setting up basic http server.

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log('package.json updated!');
+import http from 'node:http';
+
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+    res.end('Server is now up!');
+})
+
+server.listen(PORT, () => console.log(`server is running on ${PORT}`));
