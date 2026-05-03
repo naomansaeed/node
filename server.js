03dc84c2791dc71071e13c91ddfe9a477491231e
Naoman Saeed <naoman.saeed@gmail.com>
2026-05-03 12:27:57 +0500

    feat: setting up first /api path.

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ import http from 'node:http';
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-    res.end('Server is now up!');
+    if (req.url === '/api' && req.method === 'GET') {
+        res.end('Server is now up!');
+    }
+    
 })
 
 server.listen(PORT, () => console.log(`server is running on ${PORT}`));
