3a063b72a483aaadaddb5251f5942e8d5673b942
Naoman Saeed <naoman.saeed@gmail.com>
2026-05-03 12:39:07 +0500

    feat: sending json data from the server.

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 import http from 'node:http';
+import { type } from 'node:os';
 
 const PORT = 8000;
 
+const animal = {
+    type: "mammel",
+    species: "elephant"
+}
+
 const server = http.createServer((req, res) => {
     if (req.url === '/api' && req.method === 'GET') {
-        res.end('Server is now up!');
+        //console.log(JSON.stringify(animal));
+        res.end(JSON.stringify(animal));
     }
     
 })
